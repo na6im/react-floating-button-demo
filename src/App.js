b677657bdddd9d6ddcedb1d9c502cc0632423a8f
na6im <nassim.amokrane@hotmail.com>
2020-04-27 03:01:25 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,136 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import FloatingButton from "react-floating-button";
+import Item from "react-floating-button";
+import logo from "./assets/DOWNLOAD.svg";
+import logo2 from "./assets/FORWARD 2.svg";
+import logo3 from "./assets/GRID.svg";
+import logo4 from "./assets/GIFT.svg";
+import logo5 from "./assets/HOME.svg";
+import logo6 from "./assets/MAP.svg";
+import logo7 from "./assets/MAP PIN.svg";
+import logo8 from "./assets/MUSIC.svg";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="App-header">
+        <FloatingButton
+            right={true}
+            top={true}
+            height={80}
+            backgroundColor={"blue"}
+            color={"black"}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Item
+              imgSrc={logo}
+              onClick={() => {
+                console.log("logo");
+              }}
+          />
+          <Item
+              imgSrc={logo2}
+              onClick={() => {
+                console.log("logo2");
+              }}
+          />
+          <Item
+              imgSrc={logo3}
+              onClick={() => {
+                console.log("logo3");
+              }}
+          />
+          <Item
+              imgSrc={logo4}
+              onClick={() => {
+                console.log("logo4");
+              }}
+          />
+          <Item
+              imgSrc={logo5}
+              onClick={() => {
+                console.log("logo5");
+              }}
+          />
+          <Item
+              imgSrc={logo6}
+              onClick={() => {
+                console.log("logo6");
+              }}
+          />
+          <Item
+              imgSrc={logo7}
+              onClick={() => {
+                console.log("logo7");
+              }}
+          />
+          <Item
+              imgSrc={logo8}
+              onClick={() => {
+                console.log("logo8");
+              }}
+          />
+        </FloatingButton>
+        <FloatingButton right={false} top={true} height={30}>
+          <Item
+              imgSrc={logo}
+              onClick={() => {
+                console.log("logo");
+              }}
+          />
+          <Item
+              imgSrc={logo2}
+              onClick={() => {
+                console.log("logo2");
+              }}
+          />
+          <Item
+              imgSrc={logo3}
+              onClick={() => {
+                console.log("logo3");
+              }}
+          />
+        </FloatingButton>
+        <FloatingButton right={false} top={false} height={200}>
+          <Item
+              imgSrc={logo}
+              onClick={() => {
+                console.log("logo");
+              }}
+          />
+          <Item
+              imgSrc={logo2}
+              onClick={() => {
+                console.log("logo2");
+              }}
+          />
+          <Item
+              imgSrc={logo3}
+              onClick={() => {
+                console.log("logo3");
+              }}
+          />
+          <Item
+              imgSrc={logo4}
+              onClick={() => {
+                console.log("logo4");
+              }}
+          />
+          <Item
+              imgSrc={logo5}
+              onClick={() => {
+                console.log("logo5");
+              }}
+          />
+        </FloatingButton>
+        <FloatingButton right={true} top={false} height={100}>
+          <Item
+              imgSrc={logo}
+              onClick={() => {
+                console.log("logo");
+              }}
+          />
+        </FloatingButton>
+      </div>
   );
 }
 
