e132a74b77e244fcfe7bcc01684f2bd9a5a59153
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-05-10 20:15:28 +0000

    Deploy na6im/react-floating-button-demo to github.com/na6im/react-floating-button-demo.git:gh-pages

---
+++ precache-manifest.b3c8b82627d3c3d9268e40fd79e294cc.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74ea7f3a92b58b0effb3cc27c2453bfb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6feb1a8b248c33ec73c8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9b73be1e1b2fde2c14fb",
+    "url": "/static/js/2.837bcb67.chunk.js"
+  },
+  {
+    "revision": "5839521b381d6ae8d4739a6544c25d5a",
+    "url": "/static/js/2.837bcb67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6feb1a8b248c33ec73c8",
+    "url": "/static/js/main.146c3492.chunk.js"
+  },
+  {
+    "revision": "c0e10669e4af3b6c8f88",
+    "url": "/static/js/runtime-main.0cf8d03b.js"
+  },
+  {
+    "revision": "7e65f1aef6ba85d69431857d1875dd51",
+    "url": "/static/media/DOWNLOAD.7e65f1ae.svg"
+  },
+  {
+    "revision": "6f01845ce49b721a9bd08cd295038234",
+    "url": "/static/media/FORWARD 2.6f01845c.svg"
+  },
+  {
+    "revision": "03d0b9097774513415a1b181538c6fe1",
+    "url": "/static/media/GIFT.03d0b909.svg"
+  },
+  {
+    "revision": "5a25150d926b51d28e42ea616f30ab19",
+    "url": "/static/media/GRID.5a25150d.svg"
+  },
+  {
+    "revision": "695748c18d65f5b90c81178ec14c0a6a",
+    "url": "/static/media/HOME.695748c1.svg"
+  },
+  {
+    "revision": "cf396ce13c20f65f02aa188ed15b585d",
+    "url": "/static/media/MAP PIN.cf396ce1.svg"
+  },
+  {
+    "revision": "a4eb782b1f07259f7ff68975dc072162",
+    "url": "/static/media/MAP.a4eb782b.svg"
+  },
+  {
+    "revision": "e7f8db9ce7e4c737685e9a164fc93867",
+    "url": "/static/media/MUSIC.e7f8db9c.svg"
+  }
+]);
